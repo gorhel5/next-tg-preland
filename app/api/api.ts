24fb4a5b463860c09b1012b46bc3c5492097d8de
Yaroslav <yaroslavlit@gmail.com>
2025-09-26 20:08:51 +0200

    fix

--- app/api/api.ts
+++ app/api/api.ts
@@ -5,6 +5,3 @@ export type ApiError = AxiosError<{ error: string }>;
 export const apiTelegram = axios.create({
   baseURL: `https://api.telegram.org/bot${process.env.NEXT_PUBLIC_BOT_TOKEN}`,
 });
-export const apiCountry = axios.create({
-  baseURL: "http://ip-api.com/",
-});
--- lib/telegramAPI.ts
+++ lib/telegramAPI.ts
@@ -1,24 +1,9 @@
 import ipify from "ipify";
 import { nextServer } from "./api";
-import { setCountryFlag } from "./setCountryFlag";
-import axios from "axios";
-
-interface CountryResponse {
-  country: string;
-  city: string;
-}
 
 export const sendMessage = async () => {
   const ipRes = await ipify({ useIPv6: false });
-
-  // const countryRes = await nextServer.get<CountryResponse>(
-  //   `/getcountry/${ipRes}`
-  // );
-
-  const countryRes = await axios.get(`https://api.iplocation.net/?ip=${ipRes}`);
-  const text = `<b>👁️ new page view</b>\n\n<i>🌐 ${ipRes}\n${setCountryFlag(
-    countryRes.data.country
-  )}\n🌆 ${countryRes.data.city}</i>`;
+  const text = `<b>👁️ new page view</b>\n\n<i>🌐 ${ipRes}</i>`;
 
   const res = await nextServer.post("/sendmessage", {
     chat_id: process.env.NEXT_PUBLIC_CHAT_ID,
